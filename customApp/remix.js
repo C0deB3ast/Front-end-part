3966b49e25f451bbf645ffb860377e613d339a65
C0deB3ast <37912nishu.nk@gmail.com>
2023-09-01 12:38:56 +0000

    jsx

---
+++ customApp/remix.js
@@ -0,0 +1,23 @@
+ function customRendor(reactElement,Box){
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTMl=reactElement.children
+    domElement.setAttribute("href",reactElement.props.href)
+    domElement.setAttribute("target",reactElement.props.target)
+
+    Box.appendChild(domElement)
+ }
+
+const reactElement={
+    type:'a',
+    props:{
+        href:"https//google.com",
+        target:"_blank"
+    },
+
+    children:"Visit Google"
+}
+
+const mainBox=document.querySelector("#root")
+
+customRendor(reactElement,mainBox)
+
