0eeb7b1f457c63246f467251ca6bdbefe31c37c8
C0deB3ast <37912nishu.nk@gmail.com>
2023-09-05 07:59:06 +0000

    sadcads

---
+++ React/myApp/src/reducer.jsx
@@ -0,0 +1,11 @@
+import { createReducer } from '@reduxjs/toolkit'
+
+const initialState{
+    number:true
+}
+
+export const custom=createReducer(initialState,{
+    changethenumber:(state,action)=>{
+        state.number = action
+    }
+})
