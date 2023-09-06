3abc5b09604051b4569be7a587093fe680af9a5f
C0deB3ast <37912nishu.nk@gmail.com>
2023-09-06 07:14:15 +0000

    yiuoiuh

--- React/myApp/src/main.jsx
+++ React/myApp/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Routing from "./router"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Routing/>
   </React.StrictMode>,
 )
---
+++ React/myApp/src/router.jsx
@@ -0,0 +1,10 @@
+import {
+     BrowserRouter as Router,
+     Routes,
+     Route
+} from "react-router-dom"
+import App from './App'
+
+const Rounting=()=>{
+    return 
+}
