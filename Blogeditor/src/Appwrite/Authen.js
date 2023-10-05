97974606e210120f3b48bf56dc373767443b6b99
C0deB3ast <37912nishu.nk@gmail.com>
2023-10-05 14:36:48 +0530

    Authen-Methods

--- Blogeditor/src/Appwrite/Authen.js
+++ Blogeditor/src/Appwrite/Authen.js
@@ -4,15 +4,36 @@ import { Client, Account, ID } from "appwrite";
 export class AuthenService {
   client = new Client();
   account;
-
-  constructor() {
+  consructor(){
     this.client
-      .setEndpoint(config.appWrite)
-      .setProject(config.appWriteProjectId);
+      .setEndpoint(config.appWriteUrl)
+      .setProject(config.appWriteProjectId)
+      this.account = new Account(this.client)
+  }
+  async createAccount({email, password,name}){ //we  here destructure services of appwrite bcs we do not want to depend on appwrite for authentication
+    //this method can be fail so for failSafe
+    try {
+      const userAccount = await this.account.create(ID.unique(),email, password, name)
+      if (userAccount) {
+        // call login method
+        return this.login({email, password})
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+       throw error
+    }
+  }
 
-    this.account = new Account(this.client);
+  async login({email, password}){
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error
+    }
   }
 }
+
 
 const authenService = new AuthenService();
 
--- Blogeditor/src/config/config.js
+++ Blogeditor/src/config/config.js
@@ -1,5 +1,5 @@
 const config ={
-appWrite:String(import.meta.env.VITE_APPWRITE_URL),
+appWriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
 appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
 appWriteDataBaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 appWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
